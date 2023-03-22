9aa64d6677094bd884dcb0c0aed233dc5a589122
siimlaas <56837653+siimlaas@users.noreply.github.com>
2023-03-22 21:29:03 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (request, response)=>{
+    
+    response.sendFile(__dirname + '/index.html');
+
+});
+
+app.get('/about', (request, response) => {
+    response.send('Hello, I am Siim. Nice to meet you')
+
+});
+
+app.listen(3000, ()=>{
+    console.log('server is running on Port 3000');
+});
+
+
